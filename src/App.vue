982ecb589adfb74d712ce3fa79b845f8f2b18e41
wangkaiwd <1162486176@qq.com>
2019-04-16 15:49:24 +0800

    feat: message组件出现位置调整

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,12 @@
 <template>
   <div class="app">
     <div class="component-wrapper">
-      <smile-button class="item" :loading="true" icon="upload">primary</smile-button>
-      <smile-button class="item" type="secondary">secondary</smile-button>
-      <smile-button class="item" type="success">success</smile-button>
-      <smile-button class="item" type="warning">warning</smile-button>
-      <smile-button class="item" disabled type="danger">danger</smile-button>
-      <smile-button
-        class="item"
-        :loading="loading"
-        @click="onClick"
-        icon-position="right"
-        icon="thumbs-up"
-        type="danger"
-      >
-        danger
-      </smile-button>
-    </div>
-    <div class="component-wrapper">
-      <smile-button-group class="item">
-        <smile-button type="secondary" icon="left">left</smile-button>
-        <smile-button type="secondary">middle</smile-button>
-        <smile-button type="secondary" icon="right">right</smile-button>
-      </smile-button-group>
-      <smile-button-group class="item">
-        <smile-button icon="left">left</smile-button>
-        <smile-button>middle</smile-button>
-        <smile-button icon="right">right</smile-button>
-      </smile-button-group>
-    </div>
-    <div class="component-wrapper">
-      <!-- v-model: 相当于为子组件传递了 :value="msg" 并且监听了子组件的input事件 @input="msg=$event" -->
-      <smile-input
-        placeholder="输入测试文字1"
-        prefix="thumbs-up"
-        v-model="msg"
-        class="item"
-        allowClear
-      >
-      </smile-input>
-      <h2 class="item" style="margin-top: 10px;margin-bottom: 10px">{{msg}}</h2>
-      <smile-input v-model="msg2" placeholder="输入测试文字2" class="item"></smile-input>
-      <smile-input
-        placeholder="输入测试文字1"
-        prefix="password"
-        v-model="msg"
-        class="item"
-      >
-      </smile-input>
-      <smile-input
-        placeholder="输入测试文字1"
-        prefix="user"
-        v-model="msg"
-        class="item"
-        error-msg="用户名错误"
-      >
-      </smile-input>
+      <smile-button @click="showMessage">上</smile-button>
+      <br>
+      <smile-button @click="showMessage1">中</smile-button>
+      <br>
+      <smile-button @click="showMessage2">下</smile-button>
+      <br>
     </div>
   </div>
 </template>
@@ -71,14 +22,16 @@
       };
     },
     mounted () {
-      this.$message({ text: '测试message文字' });
     },
     methods: {
-      onClick () {
-        this.loading = true;
-        setTimeout(() => {
-          this.loading = false;
-        }, 2000);
+      showMessage () {
+        this.$message({ text: '测试message文字', duration: 2000 });
+      },
+      showMessage1 () {
+        this.$message({ text: '测试文字', duration: 2000, position: 'middle' });
+      },
+      showMessage2 () {
+        this.$message({ text: '测试文字', duration: 2000, position: 'bottom' });
       }
     }
   };
@@ -86,10 +39,10 @@
 <style lang="scss" scoped>
   .app {
     .component-wrapper {
-      margin-top: 20px;
+      margin: 20px;
     }
     .item {
-      margin-left: 20px;
+
     }
   }
 </style>
--- src/plugins/message.vue
+++ src/plugins/message.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="smile-message">
-    <slot></slot>
+  <div class="smile-message" :class="position">
+    <div class="smile-message-wrapper">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,7 +11,37 @@
     name: 'SmileMessage',
     props: {
       text: { type: String, required: true },
+      autoClose: { type: Boolean, default: true },
+      position: {
+        type: String,
+        default: 'top',
+        validator (val) {
+          return ['top', 'middle', 'bottom'].includes(val);
+        }
+      },
+      duration: { type: Number, default: 3000 },
       showClose: { type: Boolean, default: false },
+    },
+    data () {
+      return {
+        visible: false
+      };
+    },
+    mounted () {
+      this.$nextTick(() => {
+        this.open();
+      });
+    },
+    methods: {
+      open () {
+        if (this.autoClose) {
+          setTimeout(this.close, this.duration);
+        }
+      },
+      close () {
+        this.$destroy();
+        this.$el.remove();
+      }
     }
   };
 </script>
@@ -17,9 +49,18 @@
 <style lang="scss" scoped>
   .smile-message {
     position: fixed;
-    top: 20px;
     left: 50%;
     transform: translateX(-50%);
+    &.top {
+      top: 20px;
+    }
+    &.bottom {
+      bottom: 20px;
+    }
+    &.middle {
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
     padding: 15px 15px 15px 20px;
     border-radius: $border-radius-md;
     min-width: 380px;
