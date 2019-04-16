f5cb72dbd676a4fa27938fdf7586d8fabd0660f4
wangkaiwd <1162486176@qq.com>
2019-04-16 18:09:44 +0800

    feat: 配置别名和favicon图标，但是没有生效

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,16 @@
 /**
  * Created by wangkai on 2019/4/16
  */
+const path = require('path');
 module.exports = {
-  title: 'smile ui',
+  title: 'Smile UI',
   description: 'Enjoy what you are doing',
   base: '/smile-ui/',
+  head: [
+    ['link', { rel: 'icon', href: '/icon.png' }]
+  ],
   chainWebpack: (config) => {
+    // vuePress中的别名
     config.resolve.alias.set('@', path.resolve(__dirname, '../../src'));
   }
 };
