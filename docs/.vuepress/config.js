1ff4c530ed11cb1f73143e5460099da30bfef723
wangkaiwd <1162486176@qq.com>
2019-04-24 21:41:04 +0800

    fix: vuePress可以省略.md扩展名，同时以/结尾的路径将会被视为*/README.md,所以跳转路径应该是/demo/test1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,8 +7,19 @@ module.exports = {
   description: 'Enjoy what you are doing',
   base: '/smile-ui/',
   head: [
-    ['link', { rel: 'icon', href: './icon.png' }]
+    ['link', { rel: 'icon', href: '/icon.png' }]
   ],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Github', link: 'https://github.com/wangkaiwd' },
+    ],
+    sidebar: {
+      '/demo/': [
+        'test1', 'test2'
+      ]
+    }
+  },
   chainWebpack: (config) => {
     // vuePress中的别名
     config.resolve.alias.set('@', path.resolve(__dirname, '../../src'));
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,7 @@
 ---
 home: true
 actionText: 快速上手 →
-actionLink: /zh/guide/
+actionLink: /demo/test1
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
