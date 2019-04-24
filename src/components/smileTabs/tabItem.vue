49d2e6c221cfa18a747824279b76818a59bf5554
wangkaiwd <1162486176@qq.com>
2019-04-24 14:37:29 +0800

    feat: tabs的动画初步添加

--- src/components/smileTabs/tabItem.vue
+++ src/components/smileTabs/tabItem.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="smile-tab-item" v-if="$parent.select === name">
-    <slot></slot>
-  </div>
+  <transition name="slide" mode="out-in">
+    <div class="smile-tab-item" v-if="$parent.select === name">
+      <slot></slot>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -16,6 +18,20 @@
 
 <style lang="scss" scoped>
   .smile-tab-item {
+    position: relative;
     padding: 1em;
+    &.slide-enter {
+      opacity: 0;
+      transform: translateX(30px);
+    }
+    &.slide-leave-to {
+      transform: translateX(-30px);
+      opacity: 0;
+    }
+    &.slide-enter-active,
+    &.slide-leave-active {
+      position: absolute;
+      transition: all 1s;
+    }
   }
 </style>
