0b2596cffaca0d58df024004e949fbce327e863e
wangkaiwd <1162486176@qq.com>
2019-04-29 22:11:15 +0800

    feat: 初步重新实现最简单的popover组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <div class="component-wrapper">
-      <smile-popover
+      <!--<smile-popover
         title="Title"
       >
         <smile-button>click</smile-button>
@@ -35,7 +35,15 @@
         <template #content>
           this is content one, this is content two
         </template>
-      </smile-popover>
+      </smile-popover>-->
+      <smile-popover1 style="">
+        <smile-button>click</smile-button>
+        <template #content>
+          <div>
+            this is popover content
+          </div>
+        </template>
+      </smile-popover1>
     </div>
   </div>
 </template>
--- src/components/smilePopover1/index.vue
+++ src/components/smilePopover1/index.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="smile-popover1">
-
+  <div class="smile-popover1" @click="onClick">
+    <slot></slot>
+    <div class="smile-popover1-content" v-if="visible">
+      <slot name="content"></slot>
+    </div>
   </div>
 </template>
 
 <script>
+  /**
+   * Popover组件的几个难点：
+   *  1. 父元素设置overflow:hidden;时该怎么办？
+   *  2. 不能阻止事件冒泡
+   *  3. 不让事件进行多余的监听
+   *
+   * popover的难点并不是说控制弹出层的显示和隐藏，而是要帮用户写好弹出层的样式，控制弹出层的位置，以及不要有多余的事件监听。
+   * 所以我们要做的最重要的功能就是帮用户写好css样式
+   */
   export default {
-    name: 'SmilePopover'
+    name: 'SmilePopover1',
+    data () {
+      return {
+        visible: false
+      };
+    },
+    methods: {
+      onClick () {
+        this.visible = !this.visible;
+      }
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-
+  .smile-popover1 {
+    display: inline-block;
+    position: relative;
+    &-content {
+      border: 2px solid red;
+      position: absolute;
+      top: 100%;
+      left: 0;
+    }
+  }
 </style>
