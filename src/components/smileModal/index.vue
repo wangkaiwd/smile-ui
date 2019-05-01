79d63ec590209d358361f0282a012539f17873ed
wangkaiwd <1162486176@qq.com>
2019-05-01 17:48:55 +0800

    feat: 模态框动画调整

--- src/components/smileModal/index.vue
+++ src/components/smileModal/index.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="smile-modal" v-if="visible">
-    <div class="smile-modal-wrapper">
-      <div class="smile-modal-title">
-        <h3>{{title}}</h3>
-        <span class="smile-modal-close-icon" @click="$emit('update:visible',false)">
+  <transition name="fade">
+    <div class="smile-modal" v-if="visible">
+      <div class="smile-modal-wrapper">
+        <div class="smile-modal-title">
+          <h3>{{title}}</h3>
+          <span class="smile-modal-close-icon" @click="$emit('update:visible',false)">
           <smile-icon
             class="smile-modal-close-icon-delete"
             icon="close-bold"
           >
           </smile-icon>
         </span>
-      </div>
-      <div class="smile-modal-content">
-        <slot name="content"></slot>
-      </div>
-      <div class="smile-modal-footer">
-        <smile-button
-          type="secondary"
-          class="smile-modal-footer-cancel"
-          @click="$emit('update:visible',false)"
-        >
-          Cancel
-        </smile-button>
-        <smile-button>OK</smile-button>
+        </div>
+        <div class="smile-modal-content">
+          <slot name="content"></slot>
+        </div>
+        <div class="smile-modal-footer">
+          <smile-button
+            type="secondary"
+            class="smile-modal-footer-cancel"
+            @click="$emit('update:visible',false)"
+          >
+            Cancel
+          </smile-button>
+          <smile-button>OK</smile-button>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -49,6 +51,21 @@
 
 <style lang="scss" scoped>
   .smile-modal {
+    &.fade-enter,
+    &.fade-leave-to {
+      opacity: 0;
+      .smile-modal-wrapper {
+        opacity: 0;
+        transform: translate(-50%, -50%) scale(0);
+      }
+    }
+    &.fade-enter-active,
+    &.fade-leave-active {
+      .smile-modal-wrapper {
+        transition: all 0.4s;
+      }
+      transition: all 0.6s;
+    }
     position: fixed;
     top: 0;
     left: 0;
@@ -56,15 +73,14 @@
     bottom: 0;
     background-color: rgba(0, 0, 0, .5);
     .smile-modal-wrapper {
-      opacity: 0;
+      opacity: 1;
       position: absolute;
       background-color: #fff;
       top: 50%;
       left: 50%;
-      width: 600px;
+      width: 40vw;
       transform: translate(-50%, -50%);
       border-radius: $border-radius-md;
-      transition: all 1s;
     }
     &-title {
       position: relative;
