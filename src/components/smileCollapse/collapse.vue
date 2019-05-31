5e144c0ba05531e1af448eb3370fba87c6d685a3
wangkaiwd <1162486176@qq.com>
2019-05-31 22:10:12 +0800

    feat: 建立手风琴组件

---
+++ src/components/smileCollapse/collapse.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="smile-collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SmileCollapse',
+  };
+</script>
+
+<style lang="scss" scoped>
+  .smile-collapse {
+
+  }
+</style>
---
+++ src/components/smileCollapse/collapseItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="smile-collapse-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SmileCollapseItem'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .smile-collapse-item {
+    
+  }
+</style>
